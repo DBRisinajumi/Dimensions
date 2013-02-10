3a2309ab31a313e96be37eee42324f4e380acc48
Juris Malinens <juris.malinens@inbox.lv>
2013-02-10 23:58:40 +0200

    added additional web pages for Dimensions demo site

---
+++ demo/table.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * CRUD for dimension tables
+ * 
+ * @author Juris Malinens <juris.malinens@inbox.lv>
+ * @author Uldis Nelsons <uldisnelsons@gmail.com>
+ */
+require 'views/split_header.php';
+?>
+<h1>NOT IMPLEMENTED YET :(</h1>
+<?php
+require 'views/split_footer.php';
