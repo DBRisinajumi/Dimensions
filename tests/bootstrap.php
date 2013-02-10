21e47325b6c5bbea0d92987ffefcdbb24126f8ba
Juris Malinens <juris.malinens@inbox.lv>
2013-02-10 23:08:46 +0200

    fixed issue with database settings in phpunit bootstrap faile

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,15 @@
 $cfg = include dirname(__FILE__).'/../demo/config.php';
 require dirname(__FILE__).'/../src/DBRisinajumi/Dimensions/DimAutoload.php';
 \DBRisinajumi\Dimensions\DimAutoload::register();
-$Database = new \mysqli(
+/*$Database = new \mysqli(
     $cfg['db']['host'],
     $cfg['db']['user'],
     $cfg['db']['password'],
     $cfg['db']['database'],
     $cfg['db']['port']
 );
-$Database->set_charset("utf8");
-define("TEST_DB_HOST", "localhost");
-define("TEST_DB", "dimension");
-define("TEST_DB_USER", "dimension");
-define("TEST_DB_PASSWORD", "dimension");
+$Database->set_charset("utf8");*/
+define("TEST_DB_HOST", $cfg['db']['host']);
+define("TEST_DB", $cfg['db']['database']);
+define("TEST_DB_USER", $cfg['db']['user']);
+define("TEST_DB_PASSWORD", $cfg['db']['password']);
